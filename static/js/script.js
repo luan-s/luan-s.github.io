622819a2259668d4151255dd3c3b631ef96c14db
luan-s <luanjs.if@gmail.com>
2015-10-24 13:57:44 -0200

    luan

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+/*skill = document.querySelector(".skills");
+photo = document.querySelector(".photo");
+
+photo.onmouseover = function(){
+	skill.style.height = "400px";
+}
+
+skill.onmouseover = function(){
+	skill.style.height = "400px";
+}
+
+skill.onmouseout = function(){
+	skill.style.height = "0px";
+}*/
